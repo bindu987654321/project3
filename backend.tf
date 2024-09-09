d4ac6e883ee4533761aec87755d322499beb93a7
bindu987654321 <93935226+bindu987654321@users.noreply.github.com>
2024-09-09 18:25:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/*terraform {
   backend "azurerm" {
     resource_group_name   = "azurerm_resource_group.demoresourcegroup.name"
     storage_account_name  = "azurestorageaccountbin"
     container_name        = "tfstate"
     key                   = "bin/terraform.tfstate"
   }
-}
+}*/
