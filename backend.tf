908d07c0a00a69a55c5046126d10d78fcf2c58e0
bindu987654321 <93935226+bindu987654321@users.noreply.github.com>
2024-09-09 18:37:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "azurerm_resource_group.demoresourcegroup.name"
+    resource_group_name   = "AzureResourceGroup"
     storage_account_name  = "azurestorageaccountbin"
     container_name        = "tfstate"
     key                   = "bin/terraform.tfstate"
