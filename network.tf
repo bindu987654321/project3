e0d906c4fb3ae355205f20a0697bf520f5f80e95
bindu987654321 <93935226+bindu987654321@users.noreply.github.com>
2024-09-10 19:34:05 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_virtual_network" "virtualnetwork" {
+resource "azurerm_virtual_network" "demovirtualnetwork" {
   name                = "terra-demo-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.demoresourcegroup.location
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "virtualnetwork" {
 resource "azurerm_subnet" "demosubnet" {
   name                 = "subnetabc"
   resource_group_name  = azurerm_resource_group.demoresourcegroup.name
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
+  virtual_network_name = azurerm_virtual_network.demovirtualnetwork.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
